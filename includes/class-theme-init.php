94b7218a32b18626bf5a0c8b5d049b8827a9cac9
kjroelke <kj.roelke@gmail.com>
2024-08-03 16:01:49 -0500

    init v1.0.0

--- includes/class-theme-init.php
+++ includes/class-theme-init.php
@@ -20,19 +20,13 @@ class Theme_Init {
 	private Asset_Handler $asset_handler;
 
 	/**
-	 * Block Handler
-	 *
-	 * @var Block_Handler $block_handler
-	 */
-	private Block_Handler $block_handler;
-
-	/**
 	 * Theme_Init constructor.
 	 */
 	public function __construct() {
 		$this->load_required_files();
 		$this->asset_handler = new Asset_Handler( true );
-		$this->block_handler = new Block_Handler();
+		new Block_Handler();
+		$this->disable_discussion();
 		add_action( 'wp_enqueue_scripts', array( $this->asset_handler, 'enqueue_assets' ) );
 		add_action( 'wp_enqueue_scripts', array( $this->asset_handler, 'dequeue_scripts' ), 40 );
 		add_action( 'after_setup_theme', array( $this, 'theme_setup' ) );
@@ -61,10 +55,58 @@ class Theme_Init {
 		}
 	}
 
+	/** Remove comments, pings and trackbacks support from posts types. */
+	private function disable_discussion() {
+		// Close comments on the front-end.
+		add_filter( 'comments_open', '__return_false', 20, 2 );
+		add_filter( 'pings_open', '__return_false', 20, 2 );
+
+		// Hide existing comments.
+		add_filter( 'comments_array', '__return_empty_array', 10, 2 );
+
+		// Remove comments page in menu.
+		add_action(
+			'admin_menu',
+			function () {
+				remove_menu_page( 'edit-comments.php' );
+			}
+		);
+
+		// Remove comments links from admin bar.
+		add_action(
+			'init',
+			function () {
+				if ( is_admin_bar_showing() ) {
+					remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+				}
+			}
+		);
+	}
+
 	/**
 	 * Theme Setup
 	 */
 	public function theme_setup() {
+		$this->add_image_sizes();
 		remove_theme_support( 'core-block-patterns' );
+	}
+
+	/**
+	 * Add custom image sizes
+	 */
+	private function add_image_sizes() {
+		$image_sizes = array(
+			'gallery-full'      => array(
+				'width'  => 3840,
+				'height' => 2160,
+			),
+			'gallery-thumbnail' => array(
+				'width'  => 600,
+				'height' => 600,
+			),
+		);
+		foreach ( $image_sizes as $name => $size ) {
+			add_image_size( $name, $size['width'], $size['height'] );
+		}
 	}
 }
