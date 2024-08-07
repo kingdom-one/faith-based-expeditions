6928e5e0e35a9e5706b716bdbdb3aa6d8a9cb1b1
kjroelke <kj.roelke@gmail.com>
2024-08-07 14:16:19 -0500

    add new patterns

---
+++ patterns/flight-schedule.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Title: Flight Schedule
+ * Slug: fbe/flight-schedule
+ * Categories: tour
+ * Post Types: tour
+ *
+ * @package KingdomOne
+ * @subpackage BlockPatterns
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Flight Schedule"},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="wp-block-heading has-text-align-center"><strong>FLIGHT SCHEDULE</strong></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:table -->
+	<figure class="wp-block-table">
+		<table class="has-fixed-layout">
+			<tbody>
+				<tr>
+					<td colspan="2">
+						<strong>Monday, May 12, 2025</strong>
+					</td>
+					<td colspan="2">
+						<strong>American Airlines #820</strong>
+					</td>
+				</tr>
+				<tr>
+					<td><strong>Depart<br></strong>Atlanta (ATL)</td>
+					<td>Mon 10:13 AM 12-May-2025</td>
+					<td><strong>Arrive<br></strong>Dallas (DFW)</td>
+					<td>Mon 11:39 AM 12-May-2025</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<strong>Monday, May 12, 2025</strong>
+					</td>
+					<td colspan="2">
+						<strong>American Airlines #50</strong>
+					</td>
+				</tr>
+				<tr>
+					<td><strong>Depart<br></strong>Dallas (DFW)</td>
+					<td>Mon 03:05 PM 12-May-2025</td>
+					<td><strong>Arrive<br></strong>London (LHR)</td>
+					<td>Tue 06:25 AM 13-May-2025</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<strong>Tuesday, May 13, 2025</strong>
+					</td>
+					<td colspan="2">
+						<strong>British Airways #1434</strong>
+					</td>
+				</tr>
+				<tr>
+					<td><strong>Depart<br></strong>London (LHR)</td>
+					<td>Tue 07:55 AM 13-May-2025</td>
+					<td><strong>Arrive<br></strong>Edinburgh (EDI)</td>
+					<td>Tue 09:20 AM 13-May-2025</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<strong>Saturday, May 24, 2025</strong>
+					</td>
+					<td colspan="2">
+						<strong>American Airlines #725</strong>
+					</td>
+				</tr>
+				<tr>
+					<td><strong>Depart<br></strong>Dublin (DUB)</td>
+					<td>Sat 11:45 AM 24-May-2025</td>
+					<td><strong>Arrive<br></strong>Charlotte (CLT)</td>
+					<td>Sat 02:45 PM 24-May-2025</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<strong>Saturday, May 24, 2025</strong>
+					</td>
+					<td colspan="2">
+						<strong>American Airlines #5175</strong>
+					</td>
+				</tr>
+				<tr>
+					<td><strong>Depart<br></strong>Charlotte (CLT)</td>
+					<td>Sat 06:05 PM 24-May-2025</td>
+					<td><strong>Arrive<br></strong>Atlanta (ATL)</td>
+					<td>Sat 07:21 PM 24-May-2025</td>
+				</tr>
+			</tbody>
+		</table>
+	</figure>
+	<!-- /wp:table -->
+</div>
+<!-- /wp:group -->
